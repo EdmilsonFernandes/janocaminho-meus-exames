a12647ea37a79a2dc17577df33195ac6f6240692
Edmilson Fernandes <edmls2008@gmail.com>
2026-08-26 11:07:23 -0300

    fix(landing): corrige digitacao de setTextO para setTexto no DecifreReal.tsx

--- packages/web/src/components/DecifreReal.tsx
+++ packages/web/src/components/DecifreReal.tsx
@@ -64,7 +64,7 @@ export const DecifreReal = () => {
 
   const testSample = () => {
     setMode('texto');
-    setTextO(SAMPLE_EXAM_TEXT);
+    setTexto(SAMPLE_EXAM_TEXT);
     setErr('');
     setLoading(true);
     setResult(null);
